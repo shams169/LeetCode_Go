66ca917048b52fcb9be7ebd0d4339e60402b3fb9
shams169 <10395460+shams169@users.noreply.github.com>
2022-10-08 12:01:33 -0600

    LongestCommonPrefix solution

--- main.go
+++ main.go
@@ -15,10 +15,65 @@ func main() {
 	*/
 
 	// Check Palindrome
-	fmt.Println(isPalindrome(121))
-	fmt.Println(isPalindrome(132231))
-	fmt.Println(isPalindrome(1009))
-	fmt.Println(isPalindrome(-109))
-	fmt.Println(isPalindrome(1))
+	/*
+		fmt.Println(isPalindrome(121))
+		fmt.Println(isPalindrome(132231))
+		fmt.Println(isPalindrome(1009))
+		fmt.Println(isPalindrome(-109))
+		fmt.Println(isPalindrome(1))
+	*/
+
+	// Roman to Integer
+	/*
+		fmt.Println(RomanToInt("III"))
+		fmt.Println(RomanToInt("IV"))
+		fmt.Println(RomanToInt("LVIII"))
+		fmt.Println(RomanToInt("V"))
+		fmt.Println(RomanToInt("VIII"))
+	*/
+
+	// LongestCommonPrefix
+
+	// fmt.Println(longestCommonPrefix([]string{"flower", "flow", "flight"}))
+	// fmt.Println(longestCommonPrefix([]string{"fxwer", "flow", "flowmerht"}))
+
+	// fmt.Println(LCP([]string{"flower", "flow", "flight"}))
+	// fmt.Println(LCP([]string{"fxwer", "flow", "flowmerht"}))
+
+	// fmt.Println(lcprefix([]string{"flower", "flow", "flight"}))
+
+	fmt.Println(lcp_sorting([]string{"flower", "flow", "flight"}))
+	fmt.Println(lcp_sorting([]string{"dog", "race", "flight"}))
 
 }
+
+// func LCP(strs []string) string {
+
+// 	minLen := len(strs[0])
+
+// 	// for _, val := range strs {
+// 	// 	if len(val) < minLen {
+// 	// 		minLen = len(val)
+// 	// 	}
+// 	// }
+
+// 	lcp := ""
+// 	var found bool = true
+// 	for i := 0; i < minLen; i++ {
+// 		currentChar := string(strs[0][i])
+
+// 		for _, val := range strs {
+// 			if string(val[i]) != currentChar {
+// 				found = false
+// 				break
+// 			}
+// 		}
+// 		if !found {
+// 			break
+// 		} else {
+// 			lcp += currentChar
+// 		}
+// 	}
+// 	return lcp
+
+// }
