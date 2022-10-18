5624c4f7c86cc573cf22d6aadcccdd84f92600d5
shams169 <10395460+shams169@users.noreply.github.com>
2022-10-18 00:38:22 -0600

    Adding the link to youtube solution

--- producExceptSelf.go
+++ producExceptSelf.go
@@ -6,6 +6,8 @@ import "fmt"
 Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
 The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
 You must write an algorithm that runs in O(n) time and without using the division operation.
+
+https://www.youtube.com/watch?v=bNvIQI2wAjk&t=2s
 */
 
 func productExSelf(nums []int) []int {
